059d32047e9c9939c116fac8aea4dc09c3ec3484
Cheehan <pockethook@npyu.eu>
2014-11-06 12:48:19 +0000

    Use brace initialization.

--- main.cpp
+++ main.cpp
@@ -12,10 +12,10 @@ using std::endl;
 int main(int argc, char** argv) {
 	try {
 		if (argc != 2) {
-			throw logic_error("Not enough arguments");
+			throw logic_error{"Not enough arguments"};
 		}
 
-		Player player {argv[1]};
+		Player player{argv[1]};
 	}
 
 	catch (exception &e) {
