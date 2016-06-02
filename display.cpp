aff44feeed72d82a16a80fc4196662794fdf0016
Cheehan <pockethook@npyu.eu>
2016-06-02 10:14:26 +0100

    Remove extra includes

--- display.cpp
+++ display.cpp
@@ -1,5 +1,4 @@
 #include "display.h"
-#include <iostream>
 #include <stdexcept>
 
 Display::Display(const unsigned width, const unsigned height) {
--- display.h
+++ display.h
@@ -1,7 +1,7 @@
 #pragma once
 #include "SDL2/SDL.h"
 #include <array>
-#include <atomic>
+#include <memory>
 
 class Display {
 private:
