18bda9f85e3c20357f3164338fd496dd38cecd1a
Jon Frydensbjerg <jonf@vip.cybercity.dk>
2022-09-14 16:12:50 +0100

    Bump version

--- main.cpp
+++ main.cpp
@@ -36,7 +36,7 @@ int main(int argc, char **argv)
         {
             std::ostringstream usage;
             usage
-                << argv[0] << " 20220913-github" << std::endl
+                << argv[0] << " 20220914-github" << std::endl
                 << std::endl
                 << "Usage: " << argv[0] << " [OPTIONS]... FILE1 FILE2" << std::endl
                 << std::endl;
