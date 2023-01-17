4f7ac67d0df07680dd8a97d63a429d6d0931c73b
Jon Frydensbjerg <jonf@vip.cybercity.dk>
2023-01-17 07:42:55 +0100

    Bump release date

--- main.cpp
+++ main.cpp
@@ -67,7 +67,7 @@ int main(int argc, char** argv) {
 
     if (args["help"] || args.count() == 0) {
       std::ostringstream usage;
-      usage << argv[0] << " 20230103-github" << std::endl << std::endl << "Usage: " << argv[0] << " [OPTIONS]... FILE1 FILE2" << std::endl << std::endl;
+      usage << argv[0] << " 20230117-github" << std::endl << std::endl << "Usage: " << argv[0] << " [OPTIONS]... FILE1 FILE2" << std::endl << std::endl;
       argagg::fmt_ostream fmt(std::cerr);
       fmt << usage.str() << argparser;
     } else {
