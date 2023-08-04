3d22bb6ab8f6054e5c45cd8b142fc17b0d763ef0
Jon Frydensbjerg <jonf@vip.cybercity.dk>
2023-08-04 11:51:46 +0200

    Display negative positions properly

--- string_utils.cpp
+++ string_utils.cpp
@@ -9,7 +9,8 @@ std::string string_join(std::vector<std::string>& strings, const std::string& de
 }
 
 std::string format_position(const float position, const bool use_compact) {
-  const float rounded_millis = std::round(position * 1000.0F);
+  const std::string sign = position < 0 ? "-" : "";
+  const float rounded_millis = std::round(std::fabs(position) * 1000.0F);
 
   const int milliseconds = rounded_millis;
   const int seconds = milliseconds / 1000;
@@ -17,13 +18,13 @@ std::string format_position(const float position, const bool use_compact) {
   const int hours = minutes / 60;
 
   if (!use_compact || minutes >= 60) {
-    return string_sprintf("%02d:%02d:%02d.%03d", hours, minutes % 60, seconds % 60, milliseconds % 1000);
+    return string_sprintf("%s%02d:%02d:%02d.%03d", sign.c_str(), minutes % 60, seconds % 60, milliseconds % 1000);
   }
   if (seconds >= 60) {
-    return string_sprintf("%02d:%02d.%03d", minutes, seconds % 60, milliseconds % 1000);
+    return string_sprintf("%s%02d:%02d.%03d", sign.c_str(), minutes, seconds % 60, milliseconds % 1000);
   }
 
-  return string_sprintf("%d.%03d", seconds, milliseconds % 1000);
+  return string_sprintf("%s%d.%03d", sign.c_str(), seconds, milliseconds % 1000);
 }
 
 inline bool ci_compare_char(char a, char b) {
