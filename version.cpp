fc0fc9e6d7a23372cde89332fe630320b36010fe
Jon Frydensbjerg <jonf@vip.cybercity.dk>
2024-06-23 08:50:07 +0200

    Bump version

--- version.cpp
+++ version.cpp
@@ -1,4 +1,4 @@
 #include "version.h"
 
-const std::string VersionInfo::version = "20240525-warsaw";
+const std::string VersionInfo::version = "20240623-baku";
 const std::string VersionInfo::copyright = "Copyright (c) 2018-2024 Jon Frydensbjerg, the video-compare community";
