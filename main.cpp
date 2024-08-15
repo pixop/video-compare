78316e3427048eed6c76ddc17ca83350376764e2
Jon Frydensbjerg <jonf@vip.cybercity.dk>
2024-08-15 21:28:31 +0200

    Improved help text

--- main.cpp
+++ main.cpp
@@ -222,7 +222,7 @@ int main(int argc, char** argv) {
                               {"display-mode", {"-m", "--mode"}, "display mode (layout), 'split' for split screen (default), 'vstack' for vertical stack, 'hstack' for horizontal stack", 1},
                               {"window-size", {"-w", "--window-size"}, "override window size, specified as [width]x[height] (e.g. 800x600, 1280x or x480)", 1},
                               {"auto-loop-mode", {"-a", "--auto-loop-mode"}, "auto-loop playback when buffer fills, 'off' for continuous streaming (default), 'on' for forward-only mode, 'pp' for ping-pong mode", 1},
-                              {"adapt-csp-mode", {"-p", "--adapt-csp-mode"}, "adapt video to sRGB display, 'off' for no conversion (default), 'on' for basic color-conversion, 'tmatch' for tone-matching mode", 1},
+                              {"adapt-csp-mode", {"-p", "--adapt-csp-mode"}, "adapt video to an sRGB display, 'off' for no conversion (default), 'on' for basic color conversion, 'tmatch' for HDR tone-matching mode", 1},
                               {"frame-buffer-size", {"-f", "--frame-buffer-size"}, "frame buffer size (e.g. 10, 70 or 150), default is 50", 1},
                               {"time-shift", {"-t", "--time-shift"}, "shift the time stamps of the right video by a user-specified number of seconds (e.g. 0.150, -0.1 or 1)", 1},
                               {"wheel-sensitivity", {"-s", "--wheel-sensitivity"}, "mouse wheel sensitivity (e.g. 0.5, -1 or 1.7), default is 1; negative values invert the input direction", 1},
@@ -239,9 +239,9 @@ int main(int argc, char** argv) {
                               {"left-hwaccel", {"--left-hwaccel"}, "left FFmpeg video hardware acceleration, specified as [type][:device?[:options?]] (e.g. 'videotoolbox' or 'vaapi:/dev/dri/renderD128')", 1},
                               {"right-hwaccel", {"--right-hwaccel"}, "right FFmpeg video hardware acceleration, specified as [type][:device?[:options?]] (e.g. 'cuda', 'cuda:1' or 'vulkan')", 1},
                               {"find-hwaccels", {"--find-hwaccels"}, "find FFmpeg video hardware acceleration types that match the provided search term (e.g. 'videotoolbox' or 'vulkan'; use \"\" to list all)", 1},
-                              {"left-peak-nits", {"--left-peak-nits"}, "left video peak luminance in nits (e.g. 400, 850 or 1000), default is 100 for SDR; enables basic color-conversion (if mode is unset)", 1},
+                              {"left-peak-nits", {"--left-peak-nits"}, "left video peak luminance in nits (e.g. 850 or 1000), default is 100 for SDR; basic color conversion is enabled if --adapt-csp-mode is not set", 1},
                               {"right-peak-nits", {"--right-peak-nits"}, "right video peak luminance in nits", 1},
-                              {"boost-luminance", {"--boost-luminance"}, "boost luminance factor (e.g. 1.6, 3 or 5.5), default is 1; enables basic color-conversion (if mode is unset)", 1},
+                              {"boost-luminance", {"--boost-luminance"}, "adjust luminance using a multiplication factor (e.g. 0.6 or 3), default is 1; basic color conversion is enabled if --adapt-csp-mode is not set", 1},
                               {"libvmaf-options", {"--libvmaf-options"}, "libvmaf FFmpeg filter options (e.g. 'model=version=vmaf_4k_v0.6.1' or 'model=version=vmaf_v0.6.1\\\\:name=hd|version=vmaf_4k_v0.6.1\\\\:name=4k')", 1},
                               {"disable-auto-filters", {"--no-auto-filters"}, "disable the default behaviour of automatically injecting filters for deinterlacing, frame rate harmonization, and rotation", 0}}};
 
