0b3e4dc71b6e8874fe2a87bb129b8358c9f80da9
Jon Frydensbjerg <frydensbjerg.jon@gmail.com>
2025-01-16 21:25:06 +0100

    Filter noisy log messages

--- side_aware_logger.cpp
+++ side_aware_logger.cpp
@@ -1,13 +1,19 @@
 #include "side_aware_logger.h"
 #include <iostream>
 #include <mutex>
+#include <unordered_map>
+#include <unordered_set>
 
 extern "C" {
 #include <libavutil/log.h>
 }
 
 static std::recursive_mutex log_mutex;
+
 thread_local Side log_side = NONE;
+
+std::unordered_map<Side, std::unordered_set<std::string>> logged_search_strings_per_side;
+std::unordered_set<std::string> search_strings = {"No accelerated colorspace conversion found from"};
 
 const char* to_string(Side side) {
   switch (side) {
@@ -21,7 +27,26 @@ const char* to_string(Side side) {
 }
 
 void sa_av_log_callback(void* ptr, int level, const char* fmt, va_list args) {
+  const std::string message(fmt);
+
+  // Check if the message starts with any of the substrings that generate log clutter
+  bool is_noise = false;
+
+  for (const auto& noise : search_strings) {
+    if (message.find(noise) != std::string::npos) {
+      is_noise = true;
+      break;
+    }
+  }
+
   std::lock_guard<std::recursive_mutex> lock(log_mutex);
+
+  // If noise and already logged, return early
+  if (is_noise) {
+    if (!logged_search_strings_per_side[log_side].insert(message).second) {
+      return;
+    }
+  }
 
   if (level <= av_log_get_level() && log_side > NONE) {
     std::cerr << "[" << to_string(log_side) << "] ";
