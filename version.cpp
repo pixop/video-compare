7dd01236e6335047db7cac1a8cc913f3860c4c69
Jon Frydensbjerg <jonf@vip.cybercity.dk>
2025-02-23 16:41:21 +0100

    Bumped version

--- version.cpp
+++ version.cpp
@@ -1,4 +1,4 @@
 #include "version.h"
 
-const std::string VersionInfo::version = "20250215-oulu";
+const std::string VersionInfo::version = "20250223-tanger";
 const std::string VersionInfo::copyright = "Copyright (c) 2018-2025 Jon Frydensbjerg, the video-compare community";
