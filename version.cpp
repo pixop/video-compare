3f0cf70ea4b279779550459c9cb824fccba753f2
Jon Frydensbjerg <frydensbjerg.jon@gmail.com>
2026-01-20 19:45:23 +0100

    Bump version

--- version.cpp
+++ version.cpp
@@ -1,4 +1,4 @@
 #include "version.h"
 
-const std::string VersionInfo::version = "20260105-riga";
+const std::string VersionInfo::version = "20260120-rennes";
 const std::string VersionInfo::copyright = "Copyright (c) 2018-2026 Jon Frydensbjerg, the video-compare community";
