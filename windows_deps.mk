0085d614a6c693372a0c4e405f41be0b5e4fcf2e
Jon Frydensbjerg <frydensbjerg.jon@gmail.com>
2026-08-28 20:46:32 +0200

    Bump FFmpeg to version 9.0.1

--- windows_deps.mk
+++ windows_deps.mk
@@ -1,9 +1,9 @@
 # Authoritative Windows dependency versions for CI, local MinGW builds,
 # and FFmpeg source manifest generation. Update version and SHA-256 values
 # together when bumping bundled Windows libraries.
-GYAN_FFMPEG_VERSION := 8.1.2
+GYAN_FFMPEG_VERSION := 9.0.1
 GYAN_FFMPEG_VARIANT := full_build-shared
-GYAN_FFMPEG_ARCHIVE_SHA256 := cba748035c21ce1431d0823c7a3a711f38616f89f87a265dceddf9b7f6749d2d
+GYAN_FFMPEG_ARCHIVE_SHA256 := cb4d5e8db6a3353bffdb2100d3eb4b76733457fa443215e236f57c99f9ffdca4
 
 SDL2_VERSION := 2.32.10
 SDL2_WIN32_X64_ZIP_SHA256 := 6cf9706eefd0a4a06dc764007934d428afaf029fabdd408a9e646048c91e18fb
